bc9ee44fb65d2db071f4c31a36c1bb236c9cd2cb
Pascal Mathis <mail@pascalmathis.com>
2019-05-29 22:42:53 +0200

    various: Add missing copyright notices

--- mod-system/uptime.go
+++ mod-system/uptime.go
@@ -1,3 +1,21 @@
+/*
+ * nagocheck - Reliable and lightweight Nagios plugins written in Go
+ * Copyright (C) 2018-2019  Pascal Mathis
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package modsystem
 
 import (
@@ -16,6 +34,8 @@ type uptimePlugin struct {
 
 type uptimeResource struct {
 	nagocheck.Resource
+
+	uptime float64
 }
 
 type uptimeContext struct {
@@ -59,16 +79,26 @@ func newUptimeResource(plugin *uptimePlugin) *uptimeResource {
 func (r *uptimeResource) Probe(warnings nagopher.WarningCollection) (metrics []nagopher.Metric, _ error) {
 	valueRange := nagopher.NewBounds(nagopher.BoundsOpt(nagopher.LowerBound(0)))
 
-	uptime, err := host.Uptime()
-	if err != nil {
+	if err := r.Collect(); err != nil {
 		return metrics, err
 	}
 
 	metrics = append(metrics,
-		nagopher.MustNewNumericMetric("uptime", float64(uptime), "s", &valueRange, ""),
+		nagopher.MustNewNumericMetric("uptime", r.uptime, "s", &valueRange, ""),
 	)
 
 	return metrics, nil
+}
+
+func (r *uptimeResource) Collect() error {
+	uptime, err := host.Uptime()
+	if err != nil {
+		return err
+	}
+
+	r.uptime = float64(uptime)
+
+	return nil
 }
 
 func newUptimeContext(plugin *uptimePlugin, warningThreshold *nagopher.Bounds, criticalThreshold *nagopher.Bounds) *uptimeContext {
